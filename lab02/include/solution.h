04f2a7694c83b4784b7b04b19048e94bc0e3545c
germanpermakov <gper9020@gmail.com>
2024-09-23 15:00:19 +0300

    lab2

--- lab02/include/solution.h
+++ lab02/include/solution.h
@@ -1,5 +1,50 @@
 #pragma once  // говорит компилятору что данный файл должен быть включен только один раз
 
 #include <string>
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
 
-std::string solution(const std::string& line);
+
+class Five {
+private:
+    unsigned char *data_;
+    size_t size_;
+    size_t capacity_;
+
+public:
+    Five();
+
+    Five(const size_t &n, unsigned char t = '0');
+
+    Five(const std::string& other);
+
+    Five(const Five &other);
+
+    Five(std::initializer_list<unsigned char> &other);
+
+    Five(Five &&other) noexcept;
+
+    Five &operator=(const Five &other);
+
+    Five &operator=(Five &&other);
+
+    size_t Size() const noexcept;
+
+    size_t Capacity() const noexcept;
+
+    void Clear() noexcept;
+
+    Five operator+(const Five &other) const;
+
+    Five operator-(const Five &other) const;
+
+    bool operator>(const Five &other) const;
+
+    bool operator<(const Five &other) const;
+
+    bool operator==(const Five &other) const;
+
+
+    virtual ~Five() noexcept;
+};
--- lab02/src/solution.cpp
+++ lab02/src/solution.cpp
@@ -1,14 +1,20 @@
 #include "../include/solution.h"
 #include <string>
 
-std::string solution(const std::string& line) {
-    std::string result = line;  // создаем копию строки
-    for (char& c : result) {    // проходим по ссылке на каждый символ
-        if (c == 'a') {
-            c = 'b';
-        } else if (c == 'b') {
-            c = 'a';
-        }
-    }
-    return result;
+Five::Five() : size_{0}, data_{nullptr}, capacity_{0} {
+    std::cout << "Default constructor" << std::endl;
 }
+
+Five::Five(const size_t& n, unsigned char t) : size_{n}, data_{new unsigned char [size_]}, capacity_{n} {
+    for (size_t i{0}; i < size_; ++i) {
+        data_[i] = t;
+    }
+}
+
+Five::Five(const std::string& other) : size_{other.size()}, data_{new unsigned char [size_]}, capacity_{size_} {
+    for (size_t i{0}; i < size_; ++i) {
+        data_[i] = other[i];
+    }
+}
+
+
